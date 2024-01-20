53616baeb1057c3da14d607683d5933d37d1aa2b
kelvin-ndoma <kelvinndomamutua@gmail.com>
2024-01-20 03:17:38 +0300

    a few styling on navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-// App.js
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './components/Home';
 import Services from './components/Services';
@@ -7,21 +6,18 @@ import Portfolio from './components/Portfolio';
 import Contact from './components/Contact';
 import Blogs from './components/Blogs';
 
-
 function App() {
   return (
-    <>
-      <Router>
-        <Header className="w-full min-h-screen bg-gray-950" />
-        <Home />
-        <Portfolio />
-        <Blogs/>
-        <Services />
-        <Contact />
-
-        
-      </Router>
-    </>
+    <Router>
+      <Header className="w-full min-h-screen bg-gray-950" />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/skills" element={<Portfolio />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/work" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/data/data.js
+++ src/data/data.js
@@ -1,34 +1,60 @@
-import Me from "../assets/me1.jpg"
 import images1 from "../assets/images1.png"
+import Image2 from "../assets/aluminium.png"
+import Image3 from "../assets/medical.png"
+import Github from "../assets/github.png"
+import GEC from "../assets/GEC.png"
+import age from "../assets/age.png"
+import Ministore from "../assets/Ministore.png"
 
 export const data=[
     {
         id:1,
         name:"Aluminium Fabricator",
-        image:images1,
+        image:Image2,
         github:"https://github.com/kelvin-ndoma",
-        live:"",
+        live:"https://henryfabricator.vercel.app/",
     },
     {
         id:2,
         name:"Medical Accessories web",
-        image:Me,
+        image:Image3,
         github:"https://github.com/kelvin-ndoma",
-        live:"",
+        live:"https://power-lektsuppliers.vercel.app/",
     },
     {
         id:3,
         name:"Tassel East Africa",
-        image:Me,
+        image:images1,
         github:"https://github.com/kelvin-ndoma",
-        live:"",
+        live:"https://tasselcakeaccessories.vercel.app/",
     },
     {
         id:4,
-        name:"React JS Application",
-        image:Me,
+        name:"React JS Portfolio",
+        image:Github,
         github:"https://github.com/kelvin-ndoma",
-        live:"",
+        live:"https://website-react-livid.vercel.app/",
+    },
+    {
+        id:5,
+        name:"Global Employment Challenge",
+        image:GEC,
+        github:"https://github.com/kelvin-ndoma",
+        live:"https://gecfinalpresentation.vercel.app/",
+    },
+    {
+        id:6,
+        name:"Age Calculator",
+        image:age,
+        github:"https://github.com/kelvin-ndoma",
+        live:"https://kelvin-ndoma.github.io/age-calculator/",
+    },
+    {
+        id:7,
+        name:"Ministore",
+        image:Ministore,
+        github:"https://github.com/kelvin-ndoma",
+        live:"https://ministore-theta.vercel.app/",
     },
 
 
